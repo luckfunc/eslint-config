c9536f4bee70ac371ffaa80f738297481d0212cf
xdd <45672942@qq.com>
2025-03-01 22:14:13 +0800

    init eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+	{
+		rules: {
+			quotes: ['error', 'single'], // 强制使用单引号
+			'jsx-quotes': ['error', 'prefer-single'], // jsx属性 强制使用单引号
+			'comma-dangle': ['error', 'never'], // 禁止对象、数组、函数参数等的尾随逗号
+			'no-multi-spaces': ['error'] // 禁止多个连续空格
+		}
+	}
+];
+
+export default eslintConfig;
